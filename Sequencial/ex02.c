2ed53c0b984e1821e506e89a257ba050045ead75
Ianco Ferraz <111300798+iancoF17@users.noreply.github.com>
2022-11-24 10:10:48 -0300

    ex02

---
+++ Sequencial/ex02.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("Caderno de poesias e um belo lugar.\nTantas coisas lindas que eu gostaria de falar.\n");
+printf("Eu falo em forma de versos para todos poderem escutar.\nAgora voce ja sabe por que os poetas passam os dias escrevendo em seus\n");
+printf("cadernos de poesias.");
+}
