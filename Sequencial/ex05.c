172e7e7d3d35d9f8f825750bac6733bf7fb353b1
Ianco Ferraz <111300798+iancoF17@users.noreply.github.com>
2022-11-24 10:54:31 -0300

    ex05

---
+++ Sequencial/ex05.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,x;
+    printf("Informe o valor de A: ");
+    scanf("%d",&a);
+    printf("Informe o valor de B: ");
+    scanf("%d",&b);
+    printf("%d + %d = %d",a,b,x = a+b);
+
+}
