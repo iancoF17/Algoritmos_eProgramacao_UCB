4b4272aa35d0496b3ec9c8ac3d5c4e391cfd3d2d
Ianco Ferraz <111300798+iancoF17@users.noreply.github.com>
2022-11-24 10:48:17 -0300

    ex04

---
+++ Sequencial/ex04.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,x;
+    printf("Informe o valor de A: ");
+    scanf("%d",&a);
+    printf("Informe o valor de B: ");
+    scanf("%d",&b);
+    x = a + b;
+    printf("Valor de A: %d + Valor de B: %d = Valor de X: %d",a,b,x);
+}
