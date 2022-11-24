301675c819ec9a2d41121f7001a74ccb96df1c7b
Ianco Ferraz <111300798+iancoF17@users.noreply.github.com>
2022-11-24 11:10:47 -0300

    ex07

---
+++ Sequencial/ex07.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+int a,b,PROD;
+    printf("Informe o valor de A: ");
+    scanf("%d",&a);
+    printf("Informe o valor de B: ");
+    scanf("%d",&b);
+    PROD = a*b;
+    printf("PROD = %d",PROD);
+}
